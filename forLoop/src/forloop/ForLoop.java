d9037ddb703dbb231f502db6c890bba8c01fff12
Jason <kyleskaleb56@gmail.com>
2024-03-02 19:01:15 +0200

    added forLoop learning file

--- forLoop/src/forloop/ForLoop.java
+++ forLoop/src/forloop/ForLoop.java
@@ -1,20 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package forloop;
 
-/**
- *
- * @author kyles
- */
-public class ForLoop {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+/*
+    In this learning file
+    i am going to do for loops
+    might nested
+    Use next() when you want to read individual words or tokens separated by whitespace.
+    Use nextLine() when you want to read the entire line of text, including spaces and special characters.
+
+*/
+
+public class ForLoop 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+        
+        System.out.print("Enter the number of columns: ");
+        int columns = scanner.nextInt();
+  
+        System.out.print("Enter the symbol you want to use: ");
+        String symbol = scanner.next();
+        
+        for (int i = 1; i <= rows; i++)
+        {
+            System.out.println();
+            
+            for (int j = 1; j <= columns; j++)
+            {
+                System.out.print(symbol);
+            }
+        }
+        System.out.println();
     }
     
 }
