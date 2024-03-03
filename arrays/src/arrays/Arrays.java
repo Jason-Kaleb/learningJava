7a60c090daf63d4db498e79cdb3cf70811616922
Jason <kyleskaleb56@gmail.com>
2024-03-03 15:18:11 +0200

    added arrays learning file

--- arrays/src/arrays/Arrays.java
+++ arrays/src/arrays/Arrays.java
@@ -1,20 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package arrays;
 
-/**
- *
- * @author kyles
- */
-public class Arrays {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+/*
+    Below i am going to 
+    use arrays both 1D 
+    and 2D arrays with
+    user input of course
+ */
+public class Arrays 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        //Below i am creating an array of size 2 with default values "", ""
+        String[] nameAge = new String[2];
+
+        System.out.print("Enter your name here: ");
+        String name = scanner.nextLine();
+        
+        nameAge[0] = name;
+
+        System.out.print("Enter your age here: ");
+        String age = scanner.nextLine();
+        
+        nameAge[1] = age;
+
+        String[][] informationStored = {
+                                            {"Name", "Age"},
+                                            {nameAge[0], nameAge[1]}
+                                        };
+
+        for (int i = 0; i < informationStored.length; i++) 
+        {
+            System.out.println();
+            for (int j = 0; j < informationStored[0].length; j++) 
+            {
+                System.out.print(informationStored[i][j] + " ");
+            }
+        }
+        System.out.println();
     }
-    
 }
+
