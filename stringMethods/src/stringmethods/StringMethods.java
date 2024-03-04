38ef8855c93788138116f07aa580da632773acf7
Jason <kyleskaleb56@gmail.com>
2024-03-04 18:36:09 +0200

    added stringMethods learning file

--- stringMethods/src/stringmethods/StringMethods.java
+++ stringMethods/src/stringmethods/StringMethods.java
@@ -1,20 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package stringmethods;
 
-/**
- *
- * @author kyles
- */
-public class StringMethods {
+        /*
+            String = reference data type that can store one
+            or more characters
+            Reference data types have access to useful methods
+        */
+        
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class StringMethods 
+{
+    public static void main(String[] args) 
+    {
+        String name = "      Jason             ";
+        
+        //boolean result = name.equalsIgnoreCase("jason");
+        //int result = name.length();
+        //char result = name.charAt(0);
+        //boolean result = name.isEmpty();
+        //boolean result = name.isBlank();
+        //String result = name.toUpperCase();
+        //String result = name.toLowerCase();
+        //String result = name.trim();
+        String result = (name.replace("o", "a")).trim();
+        
+        System.out.println("result = " + result);
     }
     
 }
