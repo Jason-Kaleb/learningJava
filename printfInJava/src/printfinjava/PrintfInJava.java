819c7e94256d905ad92d72d91a1647375f9605a1
Jason <kyleskaleb56@gmail.com>
2024-03-09 15:19:44 +0200

    updated learning java file - printfInJava

--- printfInJava/src/printfinjava/PrintfInJava.java
+++ printfInJava/src/printfinjava/PrintfInJava.java
@@ -1,20 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package printfinjava;
 
-/**
- *
- * @author kyles
- */
-public class PrintfInJava {
+/*
+    printf(): an optional method to control, format and
+    display text to the console window.
+    Two arguments: format + string + (object/variable/value)
+    % [flags] [precision] [width] [conversion-character]
+*/
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class PrintfInJava 
+{
+    public static void main(String[] args) 
+    {
+        boolean myBoolean = true;
+        char myChar = '@';
+        String myString = "Doe";
+        int myInt = 22;
+        double myDouble = 1.12;
+        
+        // [conversion-character]
+        System.out.printf("%b\n", myBoolean);
+        System.out.printf("%c\n", myChar);
+        System.out.printf("%s\n", myString);
+        System.out.printf("%d\n", myInt);
+        System.out.printf("%f\n", myDouble);
+        
+        // [width] -> minimum number of characters to be written as output
+        System.out.printf("Hello%10s\n", myString);
+        
+        // [[precision] -> sets number of digits of precision when outputting float-point values
+        System.out.printf("%.2f\n", myDouble);
+        
     }
     
 }
