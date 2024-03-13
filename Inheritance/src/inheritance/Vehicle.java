c432655457d0dc738b454f59a1a4b4625dca18ba
Jason <kyleskaleb56@gmail.com>
2024-03-13 18:34:20 +0200

    added class files

---
+++ Inheritance/src/inheritance/Vehicle.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Vehicle
+{
+    double speed;
+    
+    void go()
+    {
+        System.out.println("The vehicle is moving");
+    }
+    
+    void stop()
+    {
+        System.out.println("The vehicle has stopped");
+    }
+}
