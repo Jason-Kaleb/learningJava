29c31c41817084356461d0d45a3d76ec644161a1
Jason <kyleskaleb56@gmail.com>
2024-03-13 18:44:05 +0200

    updated learning java files and classes

--- Inheritance/src/inheritance/Car.java
+++ Inheritance/src/inheritance/Car.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package inheritance;
 
-/**
- *
- * @author kyles
- */
-public class Car {
-    
+/*
+    We use the keyword extends and
+    than the class name of the class
+    we want to inherit. like below
+*/
+
+public class Car extends Vehicle 
+{
+    // Car will now have access to the Vehicles class attributes and methods
+    // Car can also have its own attributes and methods in addition to the inherited attributes and methods
+    int doors = 4;
+    int wheels = 4;
 }
--- Inheritance/src/inheritance/Inheritance.java
+++ Inheritance/src/inheritance/Inheritance.java
@@ -1,8 +1,33 @@
 package inheritance;
 
+/*
+    Program: going to cover
+    inheritance. Inheritance = 
+    the process where one class acquires
+    the attributes and methods of another.
+*/
+
 public class Inheritance 
 {
     public static void main(String[] args)
     {
+        Car bmw = new Car();
+        Bicycle bike = new Bicycle();
+        
+        System.out.println(bmw.speed);
+        bmw.go();
+        bmw.stop();
+        System.out.println("Number of doors: " + bmw.doors);
+        System.out.println("Number of wheels: " + bmw.wheels);
+        System.out.println();
+        
+        System.out.println();
+        System.out.println(bike.speed);
+        bike.go();
+        bike.stop();
+        System.out.println("Number of pedals: " + bike.pedals);
+        System.out.println("Number of wheels: " + bike.wheels);
+        System.out.println();
+        
     }
 }
