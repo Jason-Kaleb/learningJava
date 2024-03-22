c4b2e68978364f1225d8ea613ca1587f3465afa3
Jason <kyleskaleb56@gmail.com>
2024-03-22 21:40:53 +0200

    updated java file

--- filesInJava/src/filesinjava/FilesInJava.java
+++ filesInJava/src/filesinjava/FilesInJava.java
@@ -1,20 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package filesinjava;
 
-/**
- *
- * @author kyles
- */
-public class FilesInJava {
+import java.io.File;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+/*
+    file: abstract representation
+    of file and directory pathnames
+*/
+
+public class FilesInJava
+{
+    public static void main(String[] args)
+    {
+        File file = new File("myFile.txt");
+        
+        if (file.exists())
+        {
+            System.out.println("The file exists!");
+            System.out.println(file.getPath());  // .getPath() method gets path
+            System.out.println(file.getAbsoluteFile()); // .getAbsoluteFile() method gets the whole path
+            System.out.println(file.isFile());
+            // file.delete(); deletes the file
+        }
+        else
+        {
+            System.out.println("The file does not exist!");
+        }
     }
-    
 }
