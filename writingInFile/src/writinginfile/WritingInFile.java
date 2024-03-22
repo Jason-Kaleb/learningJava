3a236f7b575ebc7f085165cdad513e66ea993abd
Jason <kyleskaleb56@gmail.com>
2024-03-22 21:56:17 +0200

    updated java file

--- writingInFile/src/writinginfile/WritingInFile.java
+++ writingInFile/src/writinginfile/WritingInFile.java
@@ -1,20 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package writinginfile;
 
-/**
- *
- * @author kyles
- */
-public class WritingInFile {
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+/*
+    FileWriter: allows you
+    to Write content
+    into a file 
+*/
+
+public class WritingInFile
+{
+    public static void main(String[] args) throws IOException
+    {
+        try
+        {
+            FileWriter writer = new FileWriter("myFile.txt");
+            File file = new File("myFile.txt");
+            
+            if (file.exists())
+            {
+                writer.write("My name is Jason\nI am a Software Developer Engineer in the making\n");
+                writer.append("--By Jason Kaleb\n");
+                writer.append("\nFile Name: " + file.getPath());
+                writer.close();
+            }
+            else
+            {
+                System.out.println("File does not exist!");
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
     
 }
