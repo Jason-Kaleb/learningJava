bc1f8171195352e6275d82c443bd763590c15b9b
Jason <kyleskaleb56@gmail.com>
2024-03-26 19:55:03 +0200

    updated java file - borderLayoutInJava.java

--- borderLayoutInJava/src/borderlayoutinjava/BorderLayoutInJava.java
+++ borderLayoutInJava/src/borderlayoutinjava/BorderLayoutInJava.java
@@ -1,5 +1,11 @@
 package borderlayoutinjava;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /*
     Layout manager = Defines the natural layout for components within a container
     3 common managers
@@ -10,6 +16,35 @@ public class BorderLayoutInJava
 {
     public static void main(String[] args)
     {
+        JFrame frame = new JFrame();
         
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout(50, 50));
+        frame.setVisible(true);
+        
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
+        
+        panel1.setBackground(Color.red);
+        panel2.setBackground(Color.blue);
+        panel3.setBackground(new Color(0xff4500));
+        panel4.setBackground(Color.green);
+        panel5.setBackground(Color.yellow);
+        
+        panel1.setPreferredSize(new Dimension(100, 100));
+        panel2.setPreferredSize(new Dimension(100, 100));
+        panel3.setPreferredSize(new Dimension(100, 100));
+        panel4.setPreferredSize(new Dimension(100, 100));
+        panel5.setPreferredSize(new Dimension(100, 100));
+        
+        frame.add(panel1, BorderLayout.NORTH);
+        frame.add(panel2, BorderLayout.SOUTH);
+        frame.add(panel3, BorderLayout.CENTER);
+        frame.add(panel4, BorderLayout.WEST);
+        frame.add(panel5, BorderLayout.EAST);
     }
 }
